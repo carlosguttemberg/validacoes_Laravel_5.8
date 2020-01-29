e2d566506de08e37f0dc89239a05ec6bd7141c18
carlosguttemberg <guttembergcarlos@gmail.com>
2020-01-28 21:06:34 -0300

    primeiro commit

---
+++ resources/views/novoCliente.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro de Cliente</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+<body>
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
